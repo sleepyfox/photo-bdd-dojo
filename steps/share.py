13896e7c2571403e6e66af43ecd6e57dd9e3f790
Fox <sleepyfox@gmail.com>
2015-04-08 20:37:08 +0100

    refactored steps to make clear which photos have been taken

--- steps/share.py
+++ steps/share.py
@@ -1,7 +1,7 @@
 from behave import *
 from unittest import *
 from camera import Camera
-from slikr import Slikr 
+from slikr import Slikr
 
 @given("that I'm logged in")
 def step_impl(context):
@@ -12,8 +12,7 @@ def step_impl(context):
 
 @when("I take an amazing photo")
 def step_impl(context):
-    context.my_photos = []
-    context.my_photos.append(context.myCamera.take_photo())
+    context.my_amazing_photo = context.myCamera.take_photo()
 
 @when("approve it (obviously)")
 def step_impl(context):
@@ -21,24 +20,24 @@ def step_impl(context):
 
 @then("I should be able to see it on my photo stream")
 def step_impl(context):
+    LATEST = -1
     photos = context.myCamera.photo_stream() # returns a list of photos
-    assert( True == (context.my_photos[-1] in photos))
+    assert( True == (context.my_amazing_photo in photos))
 
 @given("that I have already published one photo")
 def step_impl(context):
-    context.my_photos = []
-    context.my_photos.append(context.myCamera.take_photo())
+    context.my_amazing_photo = context.myCamera.take_photo()
 
 @when("I take another photo")
 def step_impl(context):
-    context.my_photos.append(context.myCamera.take_photo())
+    context.my_other_amazing_photo = context.myCamera.take_photo()
 
 @then("I should see the new photo on my photo stream")
 def step_impl(context):
     photos = context.myCamera.photo_stream() # returns a list of photos
-    assert( True == (2 in photos))
+    assert( True == (context.my_other_amazing_photo in photos))
 
 @then("I should be able to see my previous photo on my photo stream")
 def step_impl(context):
     photos = context.myCamera.photo_stream() # returns a list of photos
-    assert( True == (1 in photos))
+    assert( True == (context.my_amazing_photo in photos))
